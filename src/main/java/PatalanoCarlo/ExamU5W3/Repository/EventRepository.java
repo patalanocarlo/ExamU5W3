7a7af566e44ae1f61d6ba71386cc7a2ae4dfb964
patalanocarlo <157119840+patalanocarlo@users.noreply.github.com>
2024-07-05 10:26:36 +0200

    Creazione repository, ora creazione e confiugrazione per le password ecc

---
+++ src/main/java/PatalanoCarlo/ExamU5W3/Repository/EventRepository.java
@@ -0,0 +1,10 @@
+package PatalanoCarlo.ExamU5W3.Repository;
+
+import PatalanoCarlo.ExamU5W3.Entites.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface EventRepository extends JpaRepository <Event, Long> {
+    List<Event> findByOrganizerId(Long organizerId);
+}
+
